f6b0225b1128c60c383589ded68c3efb024fb087
Delmar-Ty <reliablesource13@gmail.com>
2021-11-05 18:23:13 -0700

    Ugh

--- Sign Up Form/JavaScript/script.js
+++ Sign Up Form/JavaScript/script.js
@@ -1,1 +1,16 @@
-//For Later
+function checker() {
+    var firstName = document.getElementById('first-name').value;
+    var lastName = document.getElementById('last-name').value;
+    var cell = document.getElementById('phone-number').value;
+    var zip = document.getElementById('zip-code').value;
+    if (validateFName(firstName)) {
+        alert("It works");
+    }
+}
+
+function validateFName (fName) {
+    var characters = /^[a-bA-B]+$/;
+    if (fName.match(chars)) {
+        return true;
+    }
+}
